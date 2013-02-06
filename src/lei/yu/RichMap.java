8fca818d6584cf5e5936177dd877f3f90f55a7e7
harolei <harolei@gmail.com>
2013-02-06 21:47:47 +0800

    #2 [harolei] Wrote the test and implement of refresh the map when the land is sold or upgrade， and refactor it.

--- src/lei/yu/RichMap.java
+++ src/lei/yu/RichMap.java
@@ -1,55 +1,47 @@
 package lei.yu;
 
 public class RichMap {
-    private Land[] land = new Land[70];
+    private Land[] landsOnTheMap = new Land[70];
     public RichMap(){
         for(int i=0;i<70;i++){
-            land[i] = new Land();
-            land[i].setLandKind("0");
+            landsOnTheMap[i] = new Land();
+            landsOnTheMap[i].setLandKind("0");
         }
-        land[0].setLandKind("S");
-        land[14].setLandKind("H");
-        land[28].setLandKind("T");
-        land[35].setLandKind("G");
-        land[49].setLandKind("P");
-        land[63].setLandKind("M");
+        landsOnTheMap[0].setLandKind("S");
+        landsOnTheMap[14].setLandKind("H");
+        landsOnTheMap[28].setLandKind("T");
+        landsOnTheMap[35].setLandKind("G");
+        landsOnTheMap[49].setLandKind("P");
+        landsOnTheMap[63].setLandKind("M");
         for(int i=64;i<70;i++){
-            land[i].setLandKind("$");
+            landsOnTheMap[i].setLandKind("$");
         }
     }
-    public String getMap() {
+    private String setMap(Land[] lands){
         String map = "";
         for(int i=0;i<29;i++){
-            map += land[i].getLandKind();
+            map += lands[i].getLandKind();
         }
-        map += "\n" + land[69].getLandKind() + "                           " + land[29].getLandKind();
-        map += "\n" + land[68].getLandKind() + "                           " + land[30].getLandKind();
-        map += "\n" + land[67].getLandKind() + "                           " + land[31].getLandKind();
-        map += "\n" + land[66].getLandKind() + "                           " + land[32].getLandKind();
-        map += "\n" + land[65].getLandKind() + "                           " + land[33].getLandKind();
-        map += "\n" + land[64].getLandKind() + "                           " + land[34].getLandKind() + "\n";
+        map += "\n" + lands[69].getLandKind() + "                           " + lands[29].getLandKind();
+        map += "\n" + lands[68].getLandKind() + "                           " + lands[30].getLandKind();
+        map += "\n" + lands[67].getLandKind() + "                           " + lands[31].getLandKind();
+        map += "\n" + lands[66].getLandKind() + "                           " + lands[32].getLandKind();
+        map += "\n" + lands[65].getLandKind() + "                           " + lands[33].getLandKind();
+        map += "\n" + lands[64].getLandKind() + "                           " + lands[34].getLandKind() + "\n";
         for(int i=63;i>34;i--){
-            map += land[i].getLandKind();
+            map += lands[i].getLandKind();
         }
         return map;
     }
+    public String getMap() {
+       String map = this.setMap(landsOnTheMap);
+        return map;
+    }
 
-    public String refreshMap(int landPosition) {
-        String map = "";
-        String landLevel = String.valueOf(land[landPosition].getLevel() + 1);
-        land[landPosition].setLandKind(landLevel);
-        for(int i=0;i<29;i++){
-            map += land[i].getLandKind();
-        }
-        map += "\n" + land[69].getLandKind() + "                           " + land[29].getLandKind();
-        map += "\n" + land[68].getLandKind() + "                           " + land[30].getLandKind();
-        map += "\n" + land[67].getLandKind() + "                           " + land[31].getLandKind();
-        map += "\n" + land[66].getLandKind() + "                           " + land[32].getLandKind();
-        map += "\n" + land[65].getLandKind() + "                           " + land[33].getLandKind();
-        map += "\n" + land[64].getLandKind() + "                           " + land[34].getLandKind() + "\n";
-        for(int i=63;i>34;i--){
-            map += land[i].getLandKind();
-        }
+    public String refreshMapWhenLandsChanged(int landPosition) {
+        String landLevel = String.valueOf(landsOnTheMap[landPosition].getLevel() + 1);
+        landsOnTheMap[landPosition].setLandKind(landLevel);
+        String map = this.setMap(landsOnTheMap);
         return map;
     }
 }
--- test/lei/yu/RichManTest.java
+++ test/lei/yu/RichManTest.java
@@ -1,14 +1,22 @@
 package lei.yu;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
 
 public class RichManTest {
+
+    private RichMap map;
+
+    @Before
+    public void setUp() throws Exception {
+        map = new RichMap();
+    }
+
     @Test
     public void should_get_string_of_map() throws Exception {
-        RichMap map = new RichMap();
         assertThat(map.getMap(),is("S0000000000000H0000000000000T" + '\n' +
                                    "$                           0" + '\n' +
                                    "$                           0" + '\n' +
@@ -21,14 +29,13 @@ public class RichManTest {
 
     @Test
     public void should_refresh_the_map_when_the_land_has_changed() throws Exception {
-        RichMap map = new RichMap();
-        assertThat(map.refreshMap(5),is("S0000100000000H0000000000000T" + '\n' +
-                                        "$                           0" + '\n' +
-                                        "$                           0" + '\n' +
-                                        "$                           0" + '\n' +
-                                        "$                           0" + '\n' +
-                                        "$                           0" + '\n' +
-                                        "$                           0" + '\n' +
-                                        "M0000000000000P0000000000000G"));
+        assertThat(map.refreshMapWhenLandsChanged(5),is("S0000100000000H0000000000000T" + '\n' +
+                                                        "$                           0" + '\n' +
+                                                        "$                           0" + '\n' +
+                                                        "$                           0" + '\n' +
+                                                        "$                           0" + '\n' +
+                                                        "$                           0" + '\n' +
+                                                        "$                           0" + '\n' +
+                                                        "M0000000000000P0000000000000G"));
     }
 }
